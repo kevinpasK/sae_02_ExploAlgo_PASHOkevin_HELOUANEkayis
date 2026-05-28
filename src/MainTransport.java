262fbb5c5369e6c21a130ae7271b313eaca7a274
Pasho Kevin <kevin.pasho7@etu.univ-lorraine.fr>
2026-05-28 15:06:37 +0200

    modif code mainTransport

--- src/MainTransport.java
+++ src/MainTransport.java
@@ -2,27 +2,19 @@ import java.util.List;
 
 public class MainTransport {
 
-    private static final String FICHIER_NOEUDS = "stan.nodes.txt";
-    private static final String FICHIER_ARCS = "stan.edges.txt";
+    private static final String FICHIER_NOEUDS = "C:/Users/kevin/Desktop/sae_algo/sae_02_ExploAlgo_PASHOkevin_HELOUANEkayis/src/stan.nodes.txt";
+    private static final String FICHIER_ARCS = "C:/Users/kevin/Desktop/sae_algo/sae_02_ExploAlgo_PASHOkevin_HELOUANEkayis/src/stan.edges.txt";
 
-
-    private static String extraireId(String arg) {
-        String regex = "\\[|\\]";
-        String[] morceaux = arg.split(regex);
-        if (morceaux.length >= 2) {
-            return morceaux[1].trim();
-        }
-        return arg.trim();
-    }
 
     public static void main(String[] args) {
-
-        if (args.length < 2) {
-            return;
+        if (args.length != 2) {
+            System.err.println("Erreur: usage java MainTransport <STATION_DEPART> <STATION_ARRIVEE>");
+            System.exit(1);
         }
 
-        String depart = args[0];
-        String arrivee = args[1];
+        String regex = "\\[|\\]";
+        String depart = args[0].split(regex)[1];
+        String arrivee = args[1].split(regex)[1];
 
         Graphe g = LireReseau.lire(FICHIER_NOEUDS, FICHIER_ARCS);
 
